f66c711a801facd8cb1e8458443d471e8726790a
Juanan Ruiz <juananruizrivas@gmail.coml.com>
2019-12-03 19:54:30 +0100

    Crea CPT y taxonomía

---
+++ include/plugin-init.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * File: kfp-formtaxon/include/plugin-init.php
+ *
+ * @package kfp_formtaxon
+ */
+
+defined( 'ABSPATH' ) || die();
+
+add_action( 'init', 'kfp_cpt_taller', 10 );
+/**
+ * Crea el CPT Taller con lo mínimo que se despacha en CPTs
+ *
+ * @return void
+ */
+function kfp_cpt_taller() {
+	$args = array(
+		'public' => true,
+		'label'  => 'Taller',
+	);
+	register_post_type( 'kfp-taller', $args );
+}
+
+/**
+ * Registra la taxonomía con lo mínimo indispensable
+ *
+ * @return void
+ */
+function kfp_taxonomy_lugares() {
+	$args = array(
+		'label'             => 'Lugar',
+		'hierarchical'      => true,
+		'public'            => true,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+	);
+	register_taxonomy( 'kfp-lugar', array( 'kfp-taller' ), $args );
+}
+add_action( 'init', 'kfp_taxonomy_lugares', 0 );
+
+/**
+ * Agrega algunos lugares de ejemplo
+ *
+ * @return void
+ */
+function kfp_lugares_add() {
+	$lugares = array(
+		'Escuela de Ingenieros Informáticos',
+		'Facultad de Derecho',
+		'Facultad de Bellas Artes',
+		'Facultad de Medicina',
+		'Rectorado',
+	);
+	foreach ( $lugares as $lugar ) {
+		wp_insert_term( $lugar, 'kfp-lugar' );
+	}
+}
+add_action( 'init', 'kfp_lugares_add', 1 );
