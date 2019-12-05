4e66348d8de44e3e90aa35b0dc985a03e332b682
Juanan Ruiz <juananruizrivas@gmail.coml.com>
2019-12-05 18:18:15 +0100

    Reorganizando las llamadas a funciones

--- include/plugin-init.php
+++ include/plugin-init.php
@@ -21,6 +21,7 @@ function kfp_cpt_taller() {
 	register_post_type( 'kfp-taller', $args );
 }
 
+add_action( 'init', 'kfp_taxonomy_lugares', 0 );
 /**
  * Registra la taxonomía con lo mínimo indispensable
  *
@@ -30,16 +31,14 @@ function kfp_taxonomy_lugares() {
 	$args = array(
 		'label'             => 'Lugar',
 		'hierarchical'      => true,
-		'public'            => true,
-		'show_ui'           => true,
 		'show_admin_column' => true,
 	);
 	register_taxonomy( 'kfp-lugar', array( 'kfp-taller' ), $args );
 }
-add_action( 'init', 'kfp_taxonomy_lugares', 0 );
 
+add_action( 'init', 'kfp_lugares_add', 1 );
 /**
- * Agrega algunos lugares de ejemplo
+ * Agrega algunos lugares de ejemplo por defecto
  *
  * @return void
  */
@@ -55,4 +54,3 @@ function kfp_lugares_add() {
 		wp_insert_term( $lugar, 'kfp-lugar' );
 	}
 }
-add_action( 'init', 'kfp_lugares_add', 1 );
